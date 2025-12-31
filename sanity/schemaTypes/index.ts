eafddd82a3a61f86d5d6c06cde8fe1ca336caa9a
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-12-31 07:00:11 -0400

    xfavor

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,13 +1,9 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import { blockContentType } from './blockContentType'
 import { categoryType } from './categoryType'
 import { productType } from './productType'
 import { promotionType } from './promotionType'
-import { contactMessageType } from './contactMessageType'
-import { postType } from './postType'
-import { authorType } from './authorType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, productType, promotionType, contactMessageType, postType, authorType],
+  types: [categoryType, productType, promotionType],
 }
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,13 +3,9 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('admin')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
+      S.documentTypeListItem('product').title('Product'),
       S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
-      ),
+      S.documentTypeListItem('promotion').title('Promotion'),
     ])
